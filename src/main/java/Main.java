5ed6366313a6ebce6ba18fc21040f5d5f9c8f8ce
JonAnderAsua <jonanderasua@gmail.com>
2021-04-01 22:24:52 +0200

    El coso que filtrea los palabros funca, todo ok.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,24 @@
 import weka.classifiers.Evaluation;
-import weka.classifiers.meta.FilteredClassifier;
 import weka.classifiers.rules.OneR;
 import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils;
-import weka.core.stemmers.LovinsStemmer;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.FixedDictionaryStringToWordVector;
 import weka.filters.unsupervised.attribute.StringToWordVector;
 import weka.filters.unsupervised.instance.Randomize;
 import weka.filters.unsupervised.instance.RemovePercentage;
 
+import java.io.File;
+import java.io.InputStream;
 import java.util.Random;
 
 public class Main {
 
     //Instantziak
-    static Instances data;
-    static Instances train;
-    static Instances test;
+    static Instances data ;
+    static Instances train ;
+    static Instances test ;
 
     //Klasifikatzaileak
     static OneR oneR = new OneR();
@@ -35,9 +37,10 @@ public class Main {
 
     //Instantziak kargatzeko metodoa
     private static void instantziakKargatu() throws Exception {
-        ConverterUtils.DataSource dataSource = new ConverterUtils.DataSource("");
+        ClassLoader cl = Main.class.getClassLoader();
+        InputStream is = cl.getResourceAsStream("dataFiles/dev/spam_dev.arff");
+        ConverterUtils.DataSource dataSource = new ConverterUtils.DataSource(is);
         data = dataSource.getDataSet();
-
         //Gure kasuan klasea lehenengo instantzian dago
         data.setClassIndex(0);
 
@@ -51,33 +54,40 @@ public class Main {
         rp.setInputFormat(data);
         rp.setPercentage(30);
 
-        Instances train = rp.useFilter(data,rp);
+        train = Filter.useFilter(data,rp);
         train.setClassIndex(0);
+
+        rp = new RemovePercentage();
+        rp.setInputFormat(data);
+        rp.setPercentage(30);
         rp.setInvertSelection(true);
 
-        Instances test = rp.useFilter(data,rp);
+        test = Filter.useFilter(data,rp);
         test.setClassIndex(0);
+        System.out.println(train.numInstances() + " " + test.numInstances());
+
     }
 
     //String to word vector filtroa
     private static void stringToWordVector() throws Exception {
         //https://stackoverflow.com/questions/41877413/how-to-use-stringtowordvector-weka-in-java
-
         //Filtroa
         StringToWordVector filter = new StringToWordVector();
-        filter.setInputFormat(data);
-        //filter.setIDFTransform(true);
-        LovinsStemmer stemmer = new LovinsStemmer();
-        filter.setStemmer(stemmer);
-        filter.setLowerCaseTokens(true);
+        filter.setInputFormat(train);
+        File fitxategia = new File("dictionary.txt");
+        filter.setDictionaryFileToSaveTo(fitxategia);
+        FixedDictionaryStringToWordVector fdstwvFilter = new FixedDictionaryStringToWordVector();
+        fdstwvFilter.setDictionaryFile(fitxategia);
+        fdstwvFilter.setInputFormat(train);
 
-        //FilteredClassifier-a sortu eta konfiguratu
-        FilteredClassifier fc = new FilteredClassifier();
-        //Filtroa zehaztu
-        fc.setFilter(filter);
-        //Base-line klasifikatzailea zehaztu
-        fc.setClassifier(oneR);
-        //Klasifikatzailea eraiki
-        fc.buildClassifier(train);
+        train = Filter.useFilter(train,filter);
+        test = Filter.useFilter(test,fdstwvFilter);
+
+        oneR.buildClassifier(train);
+        Evaluation eval = new Evaluation(test);
+
+        eval.crossValidateModel(oneR,test,10,new Random());
+        System.out.println(eval.toMatrixString());
+
     }
 }
