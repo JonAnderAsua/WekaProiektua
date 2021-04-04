f9690d1d3120f7b995c1a878bc384cb85c55b3a5
Ander <asanjuan017@ikasle.ehu.eus>
2021-04-04 23:07:42 +0200

    literalmente una linea de comentario

--- src/main/java/EntregaBi/ParametroEkorketa.java
+++ src/main/java/EntregaBi/ParametroEkorketa.java
@@ -8,6 +8,8 @@ public class ParametroEkorketa {
 
         mi idea seria cargar un modelo (el que creamos con GetRandomForestModel) y hacer parametro ekorketa
 
+        Nueva idea (y creo que mejor), hacer parametro ekorketa y luego con los resultados confijugar los parametros en GetRandomForestModel
+
      */
 
 }
