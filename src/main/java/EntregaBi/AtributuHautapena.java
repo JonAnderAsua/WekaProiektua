9330a343fff58650b3c32a706a01595aad29bda7
Ander <elsanjuyt@gmail.com>
2021-04-16 14:15:15 +0200

    He cambio los sysout de las clases para que muestren la erabilera que toca si no se han metido bien los argumentos
    Tambien he actualizado un ejecutable y añadido a cada uno un readme (estan vacios)

--- src/main/java/EntregaBi/AtributuHautapena.java
+++ src/main/java/EntregaBi/AtributuHautapena.java
@@ -22,7 +22,8 @@ public class AtributuHautapena {
     public static void main(String[] args) throws Exception {
         if(args.length  !=3) {
             System.out.println("Ez duzu arguments atala behar bezala bete!");
-            /*
+            System.out.println("Erabilera:");
+            System.out.println("java -jar AtributuHautapena.jar train.arff hiztegia dev/test.arff ");            /*
                 1. Parametroa BoW motako train arff fitxategia
                 2. Parametroa Hiztegia gordetzeko fitxategia
                 3. Dev/Test .arff fitxategia
